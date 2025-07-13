c37c6347b7f6a86e87e5a5458a50a4769268ec09
jason13official <jason13official@gmail.com>
2025-07-13 16:28:51 -0400

    Update README.md and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
 use opentale_inventory::app::create_app;
-use opentale_inventory::world::item::items::*;
 
 fn main() {
-    for (id, item) in ITEMS {
-        println!("{} -> {:?}", id, item.properties);
-    }
-
-    println!("DIAMOND durability: {:?}", RING.properties.durability);
-
     create_app().run();
-
-    println!("Closed the inventory demo successfully.");
 }
